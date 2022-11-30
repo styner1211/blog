dbd76d97dfa30bd3fb0694d66e3c32e97366cc8f
styner1211 <styner1211@gmail.com>
2022-12-01 01:40:16 +0900

    del getServerSideProps and add revalidate option

--- pages/[[...path]].js
+++ pages/[[...path]].js
@@ -92,7 +92,64 @@ export default function Post({
   );
 }
 
-export async function getServerSideProps(context) {
+// 데이터에 따라 pre-rendering할 페이지의 동적 경로를 지정하는 함수
+// getStaticPaths는 paths를 getStaticProps 로 리턴
+// { params: { path: [""] } } => path: [...path].js
+export async function getStaticPaths() {
+  const path = require("path"); // LOOK HERE
+  const filePath = path.resolve("pages");
+  const nodeMap = getNodeMap(filePath); ////
+  const mappings = toMappings(nodeMap);
+  const paths = [{ params: { path: [""] } }]; // [[...path]].js의 루트 패스는 ""
+  if (mappings) {
+    for (const e1 of mappings) {
+      const header = e1.header.path;
+      const path1 = [header];
+      paths.push({ params: { path: path1 } });
+      for (const e2 of e1.side_bar) {
+        const sidebar = e2.path;
+        const path2 = [header, sidebar];
+        paths.push({ params: { path: path2 } });
+        for (const e3 of e2.sub) {
+          const sub = e3.path;
+          const path3 = [header, sidebar, sub];
+          paths.push({ params: { path: path3 } });
+        }
+      }
+    }
+  }
+
+  return {
+    paths: paths,
+    fallback: true,
+  };
+}
+
+// 빌드 시 데이터를 패치하는 함수
+// Next.js가 빌드시 알아서 함수를 실행하고, 그 props를 컴포넌트에 전달
+// params: getStaticPaths로 부터 전달 받음
+export async function getStaticProps({ params }) {
+  let props = { post: null, path: "/" };
+  if (params.path) {
+    // 여기서 path는 [[...path]]
+    const arr = [];
+    params.path.forEach((e) => arr.push(e));
+
+    const basePath = path.resolve("pages");
+    const currPath = arr.join("/");
+    const asPath = basePath + path.sep + currPath;
+
+    let post = null;
+    if (fs.existsSync(asPath + path.sep + "post.md")) {
+      post = fs.readFileSync(asPath + path.sep + "post.md").toString();
+    }
+
+    props["path"] = currPath;
+    if (post) {
+      props["post"] = post;
+    }
+  }
+
   const { runReportWithAggregations } = require("../api/ga.js"); // write server-only code by creating a getInitialProps() method
   const tommorrow = getDateStringWithDiff(1);
   const yesterday = getDateStringWithDiff(-1);
@@ -102,12 +159,14 @@ export async function getServerSideProps(context) {
   );
   const todayTotalCount = await runReportWithAggregations(yesterday, tommorrow);
 
+  props["visitors"] = {
+    today: todayTotalCount,
+    total: cumulativeTotalCount,
+  };
+
+  props["revalidate"] = 60;
+
   return {
-    props: {
-      visitors: {
-        today: todayTotalCount,
-        total: cumulativeTotalCount,
-      },
-    },
+    props, // 이것도 _app.js의 pageProps로 분해되어 전달된다.
   };
 }
