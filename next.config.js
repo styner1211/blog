c194351a1cc714770ed0883beb65c93d3ac7abfc
styner1211 <styner1211@gmail.com>
2022-12-23 02:03:55 +0900

    revise base path

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
-const debug = process.env.NODE_ENV !== "production";
-const name = ""; // /blog
+//const debug = process.env.NODE_ENV !== "production";
+const debug = true;
+const name = "blog";
 const path = require("path");
 
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  basePath: !debug ? `${name}` : "",
-  assetPrefix: !debug ? `${name}/` : "",
+  basePath: !debug ? `/${name}` : "",
+  assetPrefix: !debug ? `/${name}/` : "",
   images: {
     loader: "akamai",
-    path: !debug ? `${name}/` : "",
+    path: !debug ? `/${name}/` : "",
   },
   future: {
     webpack5: true, // by default, if you customize webpack config, they switch back to version 4.
