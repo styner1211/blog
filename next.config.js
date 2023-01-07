6e2d7be1619fd7b548e45046ebadb0878e61e07d
styner1211 <styner1211@gmail.com>
2023-01-08 00:13:01 +0900

    base path

--- next.config.js
+++ next.config.js
@@ -7,10 +7,10 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   basePath: !debug ? `/${name}` : "",
-  assetPrefix: !debug ? `/${name}/` : "",
+  assetPrefix: !debug ? `/${name}` : "",
   images: {
     loader: "akamai",
-    path: !debug ? `/${name}/` : "",
+    path: !debug ? `/${name}` : "",
   },
   future: {
     webpack5: true, // by default, if you customize webpack config, they switch back to version 4.
