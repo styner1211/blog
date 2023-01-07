aec0dd693478570c2db16d8111792d3fb48f6e40
styner1211 <styner1211@gmail.com>
2023-01-08 00:20:34 +0900

    base path

--- env-config.js
+++ env-config.js
@@ -1,8 +1,9 @@
-const debug = process.env.NODE_ENV !== "production";
+// const debug = process.env.NODE_ENV !== "production";
+const debug = true;
 const name = "blog";
 
 module.exports = {
   "process.env.BACKEND_URL": !debug ? `/${name}` : "",
   "process.env.BASE_PATH": !debug ? `${name}` : "",
-  "process.env.ASSET_PREFIX": !debug ? `/${name}/` : ""
+  "process.env.ASSET_PREFIX": !debug ? `/${name}` : "",
 };
--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  basePath: !debug ? `/${name}` : "",
-  assetPrefix: !debug ? `/${name}` : "",
+  // basePath: !debug ? `/${name}` : "",
+  // assetPrefix: !debug ? `/${name}` : "",
   images: {
     loader: "akamai",
     path: !debug ? `/${name}` : "",
